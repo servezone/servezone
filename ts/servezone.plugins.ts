0fa90d9abea4f422964b9c34f5d896b3c25817f1
Phil Kunz <gitlab@philkunz.com>
2019-08-07 20:14:28 +0200

    fix(core): update

--- ts/servezone.plugins.ts
+++ ts/servezone.plugins.ts
@@ -1,28 +1,19 @@
-import * as beautylog from 'beautylog';
-import * as cloudflare from '@mojoio/cloudflare';
-import * as lik from 'lik';
+import * as lik from '@pushrocks/lik';
 import * as path from 'path';
-import * as q from 'smartq';
+import * as smartpromise from '@pushrocks/smartpromise';
 import * as serveZoneInterfaces from '@servezone/servezone-interfaces';
 import * as smartapp from '@servezone/appjson';
-import * as smartcli from '@pushrocks/smartcli';
-import * as smartdata from '@pushrocks/smartdata';
 import * as smartdelay from '@pushrocks/smartdelay';
 import * as smartfile from '@pushrocks/smartfile';
-import * as smartsocket from 'smartsocket';
 import * as taskbuffer from '@pushrocks/taskbuffer';
 
 export {
-  beautylog,
-  cloudflare,
   lik,
   path,
-  q,
   serveZoneInterfaces,
   smartapp,
-  smartcli,
   smartdelay,
   smartfile,
-  smartsocket,
+  smartpromise,
   taskbuffer
 };
